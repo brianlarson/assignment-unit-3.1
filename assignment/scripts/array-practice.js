87bfd4a1a731ca6454f86b6ab4284482a0542723
Brian Larson <brian@brianlarson.com>
2024-10-08 09:38:35 -0500

    Make second and last animal vars and log to console

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -39,6 +39,8 @@ console.log('First animal is', firstAnimal);
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
+let secondAnimal = animalArray[1];
+console.log('Second animal is', secondAnimal);
 
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
@@ -46,6 +48,8 @@ console.log('First animal is', firstAnimal);
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
+let lastAnimal = animalArray[3];
+console.log('Last animal is', lastAnimal);
 
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length
 //       property, rather than the exact (hardcoded) index number of the last item.
